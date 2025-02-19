59048cccde09b73978c701aaba921b64c14136e2
Purrfict <adedejikehinde2004@gmail.com>
2025-02-19 21:01:35 +0000

    updated booking management

--- ghr-frontend/src/admin/BookingManagement.jsx
+++ ghr-frontend/src/admin/BookingManagement.jsx
@@ -22,7 +22,7 @@ const BookingManagement = () => {
   const [allAvailability, setAllAvailability] = useState({});
   const [availabilityTree, setAvailabilityTree] = useState({});
 
-  const API_URL = "http://localhost:8000";
+  const API_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:8000";
   const token = localStorage.getItem("token");
   const navigate = useNavigate();
 
